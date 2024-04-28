b7b50e0c87b17fcda3d99cc92dddbd3d24c62c96
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-04-28 16:05:43 +0900

    feat: impl websocket sender

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ dayjs.extend(timezone);
 dayjs.tz.setDefault('Asia/Tokyo');
 
 export default {
-	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
+	async scheduled(_event: ScheduledEvent, env: Env, _ctx: ExecutionContext): Promise<void> {
 		if ((await env.kv.get('next-fetch')) === null) {
 			await env.kv.put('next-fetch', '0', { type: 'text' });
 		}
@@ -38,12 +38,102 @@ export default {
 			},
 		});
 
-		const notifications = res.data;
+		const notifications = res.data.sort((a, b) => {
+			return dayjs(a.updated_at).isBefore(dayjs(b.updated_at)) ? -1 : 1;
+		});
 		const pollInterval = res.headers['X-Poll-Interval'] ?? 60;
 
-		// TODO: Send Webhook
-		console.log('notifications', notifications);
-		console.log('pollInterval', pollInterval);
+		for (const notification of notifications) {
+			console.log('notification', notification);
+
+			let emoji = 'question';
+			let link = `<${notification.repository.html_url}|${notification.repository.full_name}>`;
+
+			if (notification.subject.type === 'PullRequest') {
+				const pr = await octokit.request(`GET /repos/{owner}/{repo}/pulls/{pull_number}`, {
+					owner: notification.repository.owner.login,
+					repo: notification.repository.name,
+					pull_number: parseInt(notification.subject.url.split('/').slice(-1)[0]),
+				});
+
+				if (pr.data.draft) {
+					emoji = 'draft-pr';
+				} else if (pr.data.merged) {
+					emoji = 'merged';
+				} else if (pr.data.state === 'open') {
+					emoji = 'pr-open';
+				} else if (pr.data.state === 'closed') {
+					emoji = 'pr-closed';
+				}
+
+				link = `<${pr.data.html_url}|${notification.repository.full_name}#${pr.data.number}>`;
+			} else if (notification.subject.type === 'Issue') {
+				const issue = await octokit.request(`GET /repos/{owner}/{repo}/issues/{issue_number}`, {
+					owner: notification.repository.owner.login,
+					repo: notification.repository.name,
+					issue_number: parseInt(notification.subject.url.split('/').slice(-1)[0]),
+				});
+
+				if (issue.data.state === 'open') {
+					emoji = 'issue-open';
+				} else if (issue.data.state === 'closed') {
+					emoji = 'issue-closed';
+				}
+
+				link = `<${issue.data.html_url}|${notification.repository.full_name}#${issue.data.number}>`;
+			}
+
+			const updated_at_unix = dayjs(notification.updated_at).unix();
+			const updated_at = dayjs(notification.updated_at).tz().format('YYYY-MM-DD HH:mm:ss');
+			const last_read_at_unix = dayjs(notification.last_read_at).unix();
+			const last_read_at = dayjs(notification.last_read_at).tz().format('YYYY-MM-DD HH:mm:ss');
+
+			// https://api.slack.com/reference/surfaces/formatting
+			// https://api.slack.com/messaging/webhooks
+			const body = {
+				blocks: [
+					{
+						type: 'header',
+						text: {
+							type: 'plain_text',
+							text: `:${emoji}: ${notification.subject.title}`,
+							emoji: true,
+						},
+					},
+					{
+						type: 'section',
+						text: {
+							type: 'mrkdwn',
+							text: `${link} (${notification.reason})`,
+						},
+					},
+					{
+						type: 'section',
+						text: {
+							type: 'mrkdwn',
+							text: `- Updated at <!date^${updated_at_unix}^{date_pretty} {time}|${updated_at}>\n- Last Read at <!date^${last_read_at_unix}^{date_pretty} {time}|${last_read_at}>`,
+						},
+					},
+					{
+						type: 'section',
+						text: {
+							type: 'mrkdwn',
+							text: `Debug: \`\`\`\n${JSON.stringify({ ...notification, repository: 'truncated' }, null, 2)}\n\`\`\``,
+						},
+					},
+				],
+			};
+
+			await fetch(env.WH_URL, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(body),
+			});
+
+			await new Promise((resolve) => setTimeout(resolve, 1000));
+		}
 
 		await env.kv.put('last-fetched', now.unix().toString(), { type: 'text' });
 		await env.kv.put('next-fetch', now.add(parseInt(pollInterval.toString()), 'seconds').unix().toString(), { type: 'text' });
