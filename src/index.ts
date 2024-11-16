719c3fb87cda68b07e6840e7c6ef98d30b8c39a0
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-11-16 18:11:08 +0900

    fix tab -> space

--- src/index.ts
+++ src/index.ts
@@ -8,163 +8,163 @@ dayjs.extend(timezone);
 dayjs.tz.setDefault('Asia/Tokyo');
 
 export default {
-	async scheduled(_event: ScheduledEvent, env: Env, _ctx: ExecutionContext): Promise<void> {
-		const now = dayjs();
+  async scheduled(_event: ScheduledEvent, env: Env, _ctx: ExecutionContext): Promise<void> {
+    const now = dayjs();
 
-		// sleeping 💤
-		const start = dayjs.tz().set('hour', 1).set('minute', 0).set('second', 0);
-		const end = dayjs.tz().set('hour', 8).set('minute', 0).set('second', 0);
-		if (now.isAfter(start) && now.isBefore(end)) {
-			console.log('Sleeping 💤');
-			return;
-		}
+    // sleeping 💤
+    const start = dayjs.tz().set('hour', 1).set('minute', 0).set('second', 0);
+    const end = dayjs.tz().set('hour', 8).set('minute', 0).set('second', 0);
+    if (now.isAfter(start) && now.isBefore(end)) {
+      console.log('Sleeping 💤');
+      return;
+    }
 
-		const lastFetchedTimeUnix = (await env.kv.get('last-fetched')) ?? '0';
-		const lastFetchedTime = dayjs.unix(parseInt(lastFetchedTimeUnix));
+    const lastFetchedTimeUnix = (await env.kv.get('last-fetched')) ?? '0';
+    const lastFetchedTime = dayjs.unix(parseInt(lastFetchedTimeUnix));
 
-		const octokit = new Octokit({ auth: env.GH_TOKEN });
+    const octokit = new Octokit({ auth: env.GH_TOKEN });
 
-		const res = await octokit
-			.request('GET /notifications', {
-				since: lastFetchedTime.toISOString(),
-				headers: {
-					'X-GitHub-Api-Version': '2022-11-28',
-					accept: 'application/vnd.github+json',
-					'If-Modified-Since': lastFetchedTime.toISOString(),
-				},
-			})
-			.then((res) => res)
-			.catch((err) => {
-				// 勝手に throw すな！
-				console.error(err);
-				return {
-					data: [],
-				};
-			});
+    const res = await octokit
+      .request('GET /notifications', {
+        since: lastFetchedTime.toISOString(),
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+          accept: 'application/vnd.github+json',
+          'If-Modified-Since': lastFetchedTime.toISOString(),
+        },
+      })
+      .then((res) => res)
+      .catch((err) => {
+        // 勝手に throw すな！
+        console.error(err);
+        return {
+          data: [],
+        };
+      });
 
-		const notifications = res.data.sort((a, b) => {
-			return dayjs(a.updated_at).isBefore(dayjs(b.updated_at)) ? -1 : 1;
-		});
+    const notifications = res.data.sort((a, b) => {
+      return dayjs(a.updated_at).isBefore(dayjs(b.updated_at)) ? -1 : 1;
+    });
 
-		for (const notification of notifications) {
-			let emoji = 'question';
-			let link = `<${notification.repository.html_url}|${notification.repository.full_name}>`;
-			const debugMsg = [];
+    for (const notification of notifications) {
+      let emoji = 'question';
+      let link = `<${notification.repository.html_url}|${notification.repository.full_name}>`;
+      const debugMsg = [];
 
-			if (notification.subject.type === 'PullRequest') {
-				const prNum = parseInt(notification.subject.url.split('/').slice(-1)[0]);
-				try {
-					const pr = await octokit.request(`GET /repos/{owner}/{repo}/pulls/{pull_number}`, {
-						owner: notification.repository.owner.login,
-						repo: notification.repository.name,
-						pull_number: prNum,
-					});
+      if (notification.subject.type === 'PullRequest') {
+        const prNum = parseInt(notification.subject.url.split('/').slice(-1)[0]);
+        try {
+          const pr = await octokit.request(`GET /repos/{owner}/{repo}/pulls/{pull_number}`, {
+            owner: notification.repository.owner.login,
+            repo: notification.repository.name,
+            pull_number: prNum,
+          });
 
-					if (pr.data.draft) {
-						emoji = 'draft-pr';
-					} else if (pr.data.merged) {
-						emoji = 'merged';
-					} else if (pr.data.state === 'open') {
-						emoji = 'pr-open';
-					} else if (pr.data.state === 'closed') {
-						emoji = 'pr-closed';
-					}
+          if (pr.data.draft) {
+            emoji = 'draft-pr';
+          } else if (pr.data.merged) {
+            emoji = 'merged';
+          } else if (pr.data.state === 'open') {
+            emoji = 'pr-open';
+          } else if (pr.data.state === 'closed') {
+            emoji = 'pr-closed';
+          }
 
-					link = `<${pr.data.html_url}|${notification.repository.full_name}#${pr.data.number}>`;
-				} catch (e) {
-					debugMsg.push(`Error: PR ${notification.repository.full_name}#${prNum}; ${e}`);
-					console.error(e);
-				}
-			} else if (notification.subject.type === 'Issue') {
-				const issueNum = parseInt(notification.subject.url.split('/').slice(-1)[0]);
-				try {
-					const issue = await octokit.request(`GET /repos/{owner}/{repo}/issues/{issue_number}`, {
-						owner: notification.repository.owner.login,
-						repo: notification.repository.name,
-						issue_number: issueNum,
-					});
+          link = `<${pr.data.html_url}|${notification.repository.full_name}#${pr.data.number}>`;
+        } catch (e) {
+          debugMsg.push(`Error: PR ${notification.repository.full_name}#${prNum}; ${e}`);
+          console.error(e);
+        }
+      } else if (notification.subject.type === 'Issue') {
+        const issueNum = parseInt(notification.subject.url.split('/').slice(-1)[0]);
+        try {
+          const issue = await octokit.request(`GET /repos/{owner}/{repo}/issues/{issue_number}`, {
+            owner: notification.repository.owner.login,
+            repo: notification.repository.name,
+            issue_number: issueNum,
+          });
 
-					if (issue.data.state === 'open') {
-						emoji = 'issue-open';
-					} else if (issue.data.state === 'closed') {
-						emoji = 'issue-closed';
-					}
+          if (issue.data.state === 'open') {
+            emoji = 'issue-open';
+          } else if (issue.data.state === 'closed') {
+            emoji = 'issue-closed';
+          }
 
-					link = `<${issue.data.html_url}|${notification.repository.full_name}#${issue.data.number}>`;
-				} catch (e) {
-					debugMsg.push(`Error: Issue ${notification.repository.full_name}#${issueNum}; ${e}`);
-					console.error(e);
-				}
-			} else if (notification.reason === 'ci_activity' && notification.subject.title.includes('failed')) {
-				emoji = 'x';
-				link = `<${notification.repository.html_url}/actions|${notification.repository.full_name}>`;
-			} else if (notification.reason === 'security_alert') {
-				emoji = 'warning';
-				link = `<${notification.repository.html_url}/security|${notification.repository.full_name}>`;
-			} else {
-				console.log(notification);
-				debugMsg.push(`Error: Unknown: ${JSON.stringify({ ...notification, repository: 'truncated' })}`);
-			}
+          link = `<${issue.data.html_url}|${notification.repository.full_name}#${issue.data.number}>`;
+        } catch (e) {
+          debugMsg.push(`Error: Issue ${notification.repository.full_name}#${issueNum}; ${e}`);
+          console.error(e);
+        }
+      } else if (notification.reason === 'ci_activity' && notification.subject.title.includes('failed')) {
+        emoji = 'x';
+        link = `<${notification.repository.html_url}/actions|${notification.repository.full_name}>`;
+      } else if (notification.reason === 'security_alert') {
+        emoji = 'warning';
+        link = `<${notification.repository.html_url}/security|${notification.repository.full_name}>`;
+      } else {
+        console.log(notification);
+        debugMsg.push(`Error: Unknown: ${JSON.stringify({ ...notification, repository: 'truncated' })}`);
+      }
 
-			const updated_at_unix = dayjs(notification.updated_at).unix();
-			const updated_at = dayjs(notification.updated_at).tz().format('YYYY-MM-DD HH:mm:ss');
+      const updated_at_unix = dayjs(notification.updated_at).unix();
+      const updated_at = dayjs(notification.updated_at).tz().format('YYYY-MM-DD HH:mm:ss');
 
-			// https://api.slack.com/reference/surfaces/formatting
-			// https://api.slack.com/messaging/webhooks
-			const body = {
-				blocks: [
-					{
-						type: 'header',
-						text: {
-							type: 'plain_text',
-							text: `:${emoji}: ${notification.subject.title}`,
-							emoji: true,
-						},
-					},
-					{
-						type: 'section',
-						text: {
-							type: 'mrkdwn',
-							text: `${link} (${notification.reason})`,
-						},
-					},
-					{
-						type: 'section',
-						text: {
-							type: 'mrkdwn',
-							text: `- Updated at <!date^${updated_at_unix}^{date_pretty} {time_secs}|${updated_at}>`,
-						},
-					},
-				],
-			};
+      // https://api.slack.com/reference/surfaces/formatting
+      // https://api.slack.com/messaging/webhooks
+      const body = {
+        blocks: [
+          {
+            type: 'header',
+            text: {
+              type: 'plain_text',
+              text: `:${emoji}: ${notification.subject.title}`,
+              emoji: true,
+            },
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `${link} (${notification.reason})`,
+            },
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `- Updated at <!date^${updated_at_unix}^{date_pretty} {time_secs}|${updated_at}>`,
+            },
+          },
+        ],
+      };
 
-			if (notification.last_read_at) {
-				const last_read_at_unix = dayjs(notification.last_read_at).unix();
-				const last_read_at = dayjs(notification.last_read_at).tz().format('YYYY-MM-DD HH:mm:ss');
-				body.blocks[2].text.text += `\n- Last Read at <!date^${last_read_at_unix}^{date_pretty} {time_secs}|${last_read_at}>`;
-			}
+      if (notification.last_read_at) {
+        const last_read_at_unix = dayjs(notification.last_read_at).unix();
+        const last_read_at = dayjs(notification.last_read_at).tz().format('YYYY-MM-DD HH:mm:ss');
+        body.blocks[2].text.text += `\n- Last Read at <!date^${last_read_at_unix}^{date_pretty} {time_secs}|${last_read_at}>`;
+      }
 
-			if (debugMsg.length > 0) {
-				body.blocks.push({
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: `Debug: \`\`\`\n${debugMsg.join('\n')}\n\`\`\``,
-					},
-				});
-			}
+      if (debugMsg.length > 0) {
+        body.blocks.push({
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Debug: \`\`\`\n${debugMsg.join('\n')}\n\`\`\``,
+          },
+        });
+      }
 
-			await fetch(env.WH_URL, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(body),
-			});
+      await fetch(env.WH_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
 
-			await new Promise((resolve) => setTimeout(resolve, 1000));
-		}
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
 
-		await env.kv.put('last-fetched', now.unix().toString());
-	},
+    await env.kv.put('last-fetched', now.unix().toString());
+  },
 };
